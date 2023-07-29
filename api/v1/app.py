1ab1e94919f360fc6cacdd5634eec99a58307f74
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-07-29 09:45:44 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -38,12 +38,4 @@ app.config['SWAGGER'] = {
 Swagger(app)
 
 
-if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+
